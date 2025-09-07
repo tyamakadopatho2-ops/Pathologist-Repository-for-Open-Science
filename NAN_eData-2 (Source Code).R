0356f991e290c93707819ed34e64535f61d40736
tyamakadopatho2-ops <tyamakadopatho2@gmail.com>
2025-09-07 11:23:04 +0900

    Update NAN_eData-2 (Source Code).R

--- NAN_eData-2 (Source Code).R
+++ NAN_eData-2 (Source Code).R
@@ -1,6 +1,17 @@
-# Copyright (c) 2025 Tetsuhiro Yamakado
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2025 Tetsuhiro Yamakado
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 ### NOTE FOR RSTUDIO USERS ###
 
@@ -3400,4 +3411,5 @@ ggplot(data.frame(width = factor(rope_grid),
 ### The R version and package information used in this analysis are available below:
 sessionInfo() 
 # R version 4.3.2 (2023-10-31 ucrt)
+
 
